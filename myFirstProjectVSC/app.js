246be60e2d9feaff696cfa1a8216cb2247c9f036
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-02-18 23:42:36 -0600

    created description p element

--- myFirstProjectVSC/app.js
+++ myFirstProjectVSC/app.js
@@ -7,6 +7,12 @@ const title = React.createElement(
 
 console.log(title);
 
+const desc = React.createElement(
+    'p',
+    null,
+    'I just learned how to create a React node and rendor it into the DOM. '
+)
+
 ReactDOM.render(
     title,
     document.getElementById('root')
