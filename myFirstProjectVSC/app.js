3efb35864cc38f8752c09be6a7f574f5c7ff736e
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-02-18 23:43:50 -0600

    added header element

--- myFirstProjectVSC/app.js
+++ myFirstProjectVSC/app.js
@@ -13,7 +13,15 @@ const desc = React.createElement(
     'I just learned how to create a React node and rendor it into the DOM. '
 )
 
-ReactDOM.render(
+const header = React.createElement(
+    'header',
+    null,
     title,
+    desc
+);
+
+
+ReactDOM.render(
+    header,
     document.getElementById('root')
 );
