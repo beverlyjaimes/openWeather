03c18b4329c47573716518fa50e7f141d129b263
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-02-20 23:11:28 -0600

    added script for babel and replaced titel and desc

--- myFirstProjectVSC/app.js
+++ myFirstProjectVSC/app.js
@@ -1,17 +1,6 @@
-const title = React.createElement(
-    'h1',
-    {id: 'main-title', title: "This is a title."},
-    'My First React Element!'
+const title = <h1>My First React Element!</h1>;
 
-);
-
-console.log(title);
-
-const desc = React.createElement(
-    'p',
-    null,
-    'I just learned how to create a React node and rendor it into the DOM. '
-)
+const desc = <p>I just learned how to create a React node and rendor it into the DOM. </p>;
 
 const header = React.createElement(
     'header',
