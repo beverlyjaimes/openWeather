5c4275da0625555085961726b23339ce43e24fdc
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-02-22 21:14:41 -0600

    refactoring code built header component

--- myFirstProjectVSC/app.js
+++ myFirstProjectVSC/app.js
@@ -1,15 +1,11 @@
-const desc = 'I just learned how to create a React node and rendor it into the DOM.';
-
-const myTitleID = 'main-tile';
-const name = 'Amy';
-
-const header =(
-    <header>
-        <h1 id= {myTitleID}> { name }'s First React Element</h1>
-        <p>{ desc}</p>
-        {/*this is a jsx comment*/}
-    </header>
-);
+function Header(){
+    return  (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    )
+}
 
 
 ReactDOM.render(
