8051c2b08dd6172eee79656f3794fa04c672e60d
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-02-22 21:22:34 -0600

    rendered header component

--- myFirstProjectVSC/app.js
+++ myFirstProjectVSC/app.js
@@ -4,11 +4,11 @@ function Header(){
             <h1>Scoreboard</h1>
             <span className="stats">Players: 1</span>
         </header>
-    )
+    );
 }
 
 
 ReactDOM.render(
-    header,
+    <Header />,
     document.getElementById('root')
 );
